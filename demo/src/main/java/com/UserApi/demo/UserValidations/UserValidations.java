edd59b3dd037050b64a4b1f3761fcaa2e8d4f175
Tupac Alejandro Ocampo Orozco <tupac.ocampo@gmail.com>
2022-02-15 22:32:52 -0300

    First batch of code. Maven and spring base template

---
+++ demo/src/main/java/com/UserApi/demo/UserValidations/UserValidations.java
@@ -0,0 +1,4 @@
+package com.UserApi.demo.UserValidations;
+
+public class UserValidations {
+}
---
+++ demo/src/test/java/com/UserApi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserApi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
